c3df5c43ed0362235850317998072100e3f2052b
Raida Karim <40337145+Raidakarim@users.noreply.github.com>
2019-03-07 19:11:59 -0800

    Add files

---
+++ our_server.R
@@ -0,0 +1,18 @@
+library("shiny")
+library("dplyr")
+library("ggplot2")
+library("maps")
+library("tidyr")
+library("mapproj")
+library("RColorBrewer")
+
+
+source("our_ui.R")
+
+options(scipen=999)
+
+
+# Define server logic for random distribution app ----
+our_server <- function(input, output) {
+}
+  
